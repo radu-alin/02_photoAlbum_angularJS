27f03e6715be1f08c92b61234f770914fa78a21e
Alin RADU <alin.radu.cristian@gmail.com>
2021-02-15 21:25:03 +0200

    --- reorganized: code;

---
+++ routes.js
@@ -0,0 +1,12 @@
+angularApp.config(function ($routeProvider) {
+  $routeProvider
+    .when('/albums', {
+      controller: 'AlbumListController',
+      templateUrl: './partials/album_list_partial.html',
+    })
+    .when('/', { redirectTo: '/albums' })
+    .when('/404_page', {
+      templateUrl: './partials/404_page_partial.html',
+    })
+    .otherwise({ redirectTo: '/' });
+});
